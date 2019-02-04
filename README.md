b9936097194c55c569360ad65e1368d9eff1da51
Noctoid <i@noctoid.com>
2019-02-04 14:09:24 -0800

    initiated a read api

--- README.md
+++ README.md
@@ -9,5 +9,20 @@ NekoGram web server is built on the great Python web framework Sanic which suppo
 
 NekoGram Frontend App is an single page javascript application written in React.
 
-## Database
+## Database Wrapper
+
+To ensure maximum data access throughput, the Database Wrapper is designed to listen RPC calls from RabbitMQ, and fetch the command from the containing json.
+
+API:
+```
+{
+    "db": "postings",
+    "query" : {
+        "method": "read",
+        "posting-id": 123456789
+    }
+}
+```
+
+Data retrieved will be in form of certain object also packed in json.
 
--- web_server/persistence/connector_base.py
+++ web_server/persistence/connector_base.py
@@ -5,11 +5,19 @@ import uuid
 from settings import Q_SERVER, Q_USERNAME, Q_PASSWORD
 
 
-class FibonacciRpcClient(object):
+sample_query = {
+    "db": "postings",
+    "query": {
+        "method": "read",
+        "posting-id": 123456789
+    }
+}
+
+class PersistenceConnector(object):
     def __init__(self):
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(
-                host=Q_SERVER,
+                host = Q_SERVER,
                 credentials=pika.PlainCredentials(Q_USERNAME, Q_PASSWORD)
             )
         )
@@ -20,16 +28,16 @@ class FibonacciRpcClient(object):
         self.callback_queue = result.method.queue
 
         self.channel.basic_consume(
-            self.on_response, no_ack=True,
-            queue=self.callback_queue)
+            self.on_response, no_ack=True, queue=self.callback_queue
+        )
 
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
-            self.response = body
+            self.on_response = body
 
-    def call(self, n):
+    def call(self, obj):
         self.response = None
-        self.corr_id = str(uuid.uuid4())
+        self.corr_id = str(uuid.uuid4)
         self.channel.basic_publish(
             exchange='',
             routing_key='rpc_queue',
@@ -37,7 +45,7 @@ class FibonacciRpcClient(object):
                  reply_to = self.callback_queue,
                  correlation_id = self.corr_id,
             ),
-            body=str(n)
+            body=str(obj)
         )
         while self.response is None:
             self.connection.process_data_events()
@@ -45,8 +53,7 @@ class FibonacciRpcClient(object):
 
 
 if __name__ == "__main__":
-    fibonacci_rpc = FibonacciRpcClient()
-
-    print(" [x] Requesting fib(30)")
-    response = fibonacci_rpc.call(30)
-    print(" [.] Got %r" % response)
+    db_rpc = PersistenceConnector()
+    print(" [x] Requesting foobar")
+    response = fibonacci_rpc.call({"foo": "bar"})
+    print(" [.] Got", response)
