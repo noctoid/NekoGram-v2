f2829ed2dad01d9868b490e86f4f8e31870323e3
Noctoid <i@noctoid.com>
2019-02-05 15:31:09 -0800

    continued update

--- web_server/persistence/connector_base.py
+++ web_server/persistence/connector_base.py
@@ -23,45 +23,6 @@ sample_query = {
 }
 
 
-# class PersistenceConnector(object):
-#     def __init__(self):
-#         self.connection = pika.BlockingConnection(
-#             pika.ConnectionParameters(
-#                 host=Q_SERVER,
-#                 credentials=pika.PlainCredentials(Q_USERNAME, Q_PASSWORD)
-#             )
-#         )
-
-#         self.channel = self.connection.channel()
-
-#         result = self.channel.queue_declare(exclusive=True)
-#         self.callback_queue = result.method.queue
-
-#         self.channel.basic_consume(
-#             self.on_response, no_ack=True, queue=self.callback_queue
-#         )
-
-#     def on_response(self, ch, method, props, body):
-#         if self.corr_id == props.correlation_id:
-#             self.response = body
-
-#     def call(self, obj):
-#         self.response = None
-#         self.corr_id = str(uuid.uuid4())
-#         self.channel.basic_publish(
-#             exchange='',
-#             routing_key='rpc_queue',
-#             properties=pika.BasicProperties(
-#                  reply_to=self.callback_queue,
-#                  correlation_id=self.corr_id,
-#             ),
-#             body=json.dumps(obj)
-#         )
-#         while self.response is None:
-#             self.connection.process_data_events()
-#         return self.response
-
-
 class AsyncPersistenceConnector:
     def __init__(self, loop):
         self.connection = None
@@ -71,14 +32,13 @@ class AsyncPersistenceConnector:
         self.loop = loop
 
     async def connect(self):
-        # self.connection = await connect_robust(
-        #     host="127.0.0.1",
-        #     login="nekogram",
-        #     password="qwer1234",
-        #     loop=loop
-        # )
-        self.connection = await connect("amqp://guest:guest@127.0.0.1/", loop=loop)
-        self.channel = await self.connection.channel(publisher_confirms=False)
+        self.connection = await connect(
+            host="192.168.0.32",
+            login="nekogram",
+            password="qwer1234",
+        )
+        # self.channel = await self.connection.channel(publisher_confirms=False)
+        self.channel = await self.connection.channel()
         self.callback_queue = await self.channel.declare_queue(
             exclusive=True
         )
@@ -98,7 +58,7 @@ class AsyncPersistenceConnector:
         self.futures[correlation_id] = future
         await self.channel.default_exchange.publish(
             Message(
-                "shit".encode(),
+                json.dumps(n).encode(),
                 content_type="text/plain",
                 correlation_id=correlation_id,
                 reply_to=self.callback_queue.name,
