6a07b8b2c88b0ee95d51eb4dda906ac26c07c9a8
noctoid <i@noctoid.com>
2019-03-01 19:06:54 -0800

    started adding jwt

--- web_server/Server.py
+++ web_server/Server.py
@@ -3,6 +3,9 @@
 from sanic import Sanic
 from sanic_cors import CORS, cross_origin
 from sanic.response import json
+from sanic_jwt import exceptions
+from sanic_jwt import initialize
+from sanic_jwt.decorators import protected
 
 import json as j
 
@@ -11,17 +14,60 @@ import asyncio
 # from connector_base import AsyncPersistenceConnector
 from Request_Handler import RequestHandler
 
+
+class User:
+
+    def __init__(self, id, username, password):
+        self.user_id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return "User(id='{}')".format(self.user_id)
+
+    def to_dict(self):
+        return {"user_id": self.user_id, "username": self.username}
+
+
+users = [User(1, "user1", "abcxyz"), User(2, "user2", "abcxyz")]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.user_id: u for u in users}
+
+
+async def authenticate(request, *args, **kwargs):
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    if not username or not password:
+        raise exceptions.AuthenticationFailed("Missing username or password.")
+
+    user = username_table.get(username, None)
+    if user is None:
+        raise exceptions.AuthenticationFailed("User not found.")
+
+    if password != user.password:
+        raise exceptions.AuthenticationFailed("Password is incorrect.")
+
+    return user
+
+
 app = Sanic()
+initialize(app, authenticate=authenticate)
 CORS(app)
 logic = RequestHandler()
 
 
 @app.route("/")
+@protected()
 async def test(request):
     return json({"Neko": "Gram!"})
 
 
+
+
 @app.route("/p/read/", methods=['POST', 'OPTIONS'])
+@protected()
 async def p_read(request):
     # form = request.form
     print(request)
