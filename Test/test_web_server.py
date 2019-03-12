f0d7aa5cbd7de63e3742486df0f742f4d55e2c49
noctoid <i@noctoid.com>
2019-03-12 06:24:25 -0700

    more progress - delete user

--- Test/test_web_server.py
+++ Test/test_web_server.py
@@ -40,6 +40,11 @@ if __name__ == "__main__":
             "Authorization": "Bearer " + token,
         }
 
+
+
+
+    print("\n\nTest Read User\n")
+
     result = json.loads(post(
         "http://127.0.0.1:8000/u/read/",
         headers,
@@ -50,6 +55,11 @@ if __name__ == "__main__":
     user = result['result']
     print(user)
 
+
+
+
+
+    print("\n\nTest Create P\n")
     pid = "00000000-1111-2222-3333-000000000000"
 
     result = post(
@@ -65,6 +75,10 @@ if __name__ == "__main__":
         "comments":[], "likes":[], "repost":[], "public": True}
     )
 
+
+
+
+    print("\n\nTest Update User\n")
     result = post(
         "http://127.0.0.1:8000/u/update/",
         headers,
@@ -76,6 +90,10 @@ if __name__ == "__main__":
         }
     )
 
+
+
+
+    print("\n\nTest Update P\n")
     result = post(
         "http://127.0.0.1:8000/p/update/",
         headers,
@@ -95,123 +113,24 @@ if __name__ == "__main__":
     )))
 
 
+
+    print("\n\nTest Delete User\n")
     result = post(
         "http://127.0.0.1:8000/p/delete/",
         headers,
         {"pid": pid}
     )
 
-    # result = json.loads(post(
-    #     "http://127.0.0.1:8000/p/read/",
-    #     headers,
-    #
-    # ))
 
     # delete temporary user
-    # result = post(
-    #     "http://127.0.0.1:8000/u/delete/",
-    #     {
-    #         "Content-Type": "application/json",
-    #         "Authorization": "Bearer "+token,
-    #     },
-    #     {"uid": user["uid"]}
-    # )
-    # print(result)
+    print("\n\nTest Delete User\n")
 
-    # tests = [
-    #     {
-    #         "url":"http://127.0.0.1:8000/p/read/",
-    #         "headers": {
-    #             "Content-Type": "application/json",
-    #             "Authorization": "Bearer "+token,
-    #         },
-    #         "data": {
-    #             "pid": "31a47ed1-2752-4482-9049-5257a6ec0962"
-    #         }
-    #     },
-    #     {
-    #         "url":"http://127.0.0.1:8000/p/read/",
-    #         "headers": {
-    #             "Content-Type": "application/json",
-    #             "Authorization": "Bearer "+token,
-    #         },
-    #         "data": {
-    #             "pid": "2128cbf7-2284-48a6-8fac-69e9b4bccc95"
-    #         }
-    #     },
-    #     {
-    #         "url": "http://127.0.0.1:8000/u/create/",
-    #         "headers": {
-    #             "Content-Type": "application/json"
-    #         },
-    #         "data": {
-    #             "username": "noctoid1",
-    #             "password": "qwer1234",
-    #             "displayName": "Noctoid"
-    #         }
-    #     },
-    #     {
-    #         "url": "http://127.0.0.1:8000/u/read/",
-    #         "headers": {
-    #             "Content-Type": "application/json",
-    #             "Authorization": "Bearer " + token,
-    #         },
-    #         'data': {
-    #             "username": "noctoid"
-    #         }
-    #     },
-    #     {
-    #         "url": "http://127.0.0.1:8000/p/read_many/",
-    #         "headers": {
-    #             "Content-Type": "application/json",
-    #             "Authorization": "Bearer " + token,
-    #         },
-    #         'data': {
-    #             "list_of_pid": [
-    #                 "2128cbf7-2284-48a6-8fac-69e9b4bccc95",
-    #                 "31a47ed1-2752-4482-9049-5257a6ec0962",
-    #             ]
-    #         }
-    #     },
-    #     {
-    #         "url": "http://127.0.0.1:8000/p/u_get_plist/",
-    #         "headers" : {
-    #             "Content-Type": "application/json",
-    #             "Authorization": "Bearer " + token,
-    #         },
-    #         'data': {
-    #             "username": "noctoid"
-    #         }
-    #     },
-    # ]
-    #
-    # answers = [
-    #     {'result': {'_id': 'ObjID',
-    #                 'comments': '0a67d109-a46b-4aef-a4b2-9b729d938802',
-    #                 'likes': '1f0e8396-aaab-4a01-8a57-64489adc91ae',
-    #                 'media_url': 'https://cdn.vox-cdn.com/thumbor/-bKrYahnwqww9sH9v2h34v9ViA0=/0x114:585x559/1200x800/filters:focal(248x297:340x389)/cdn.vox-cdn.com/uploads/chorus_image/image/57272301/Screen_Shot_2017_10_23_at_10.16.32_AM.0.png',
-    #                 'mime': 'image/png',
-    #                 'pid': '31a47ed1-2752-4482-9049-5257a6ec0962',
-    #                 'uid': '1234567',
-    #                 'valid': True},
-    #      'status': 'done'},
-    #     {'result': {'_id': 'ObjID',
-    #                 'comments': '23741d10-1098-49d0-8f48-b0304a5a4b25',
-    #                 'likes': '69fceafa-3f22-4f75-8339-a988ac802248',
-    #                 'media_url': 'https://cdn.vox-cdn.com/thumbor/-bKrYahnwqww9sH9v2h34v9ViA0=/0x114:585x559/1200x800/filters:focal(248x297:340x389)/cdn.vox-cdn.com/uploads/chorus_image/image/57272301/Screen_Shot_2017_10_23_at_10.16.32_AM.0.png',
-    #                 'mime': 'image/png',
-    #                 'pid': '2128cbf7-2284-48a6-8fac-69e9b4bccc95',
-    #                 'uid': '1234567',
-    #                 'valid': True},
-    #      'status': 'done'},
-    #     {'result': {'status': 200}, 'status': 'done'},
-    #     {},
-    #     {},
-    #     {}
-    #
-    # ]
-    #
-    #
-    # for t,a in zip(tests, answers):
-    #     pprint.pprint(json.loads(post(t["url"], t['headers'], t['data'])))
-    #     # assert json.loads(post(t["url"], t['headers'], t['data'])) == a
+    result = post(
+        "http://127.0.0.1:8000/u/delete/",
+        {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer "+token,
+        },
+        {"uid": user["uid"]}
+    )
+    print(result)
